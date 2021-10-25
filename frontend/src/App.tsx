55dbccb9ab55222f2a50d3c112b399f4a67376b2
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-10-25 02:04:37 +0200

    :tada: added frontend
    Finished the frontend web aplication for dowhile api

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { LoginBox } from './components/LoginBox';
+import { MessageList } from './components/MessageList';
+
+import styles from './App.module.scss';
+import { useContext } from 'react';
+import { AuthContext } from './contexts/auth';
+import { SendMessageForm } from './components/SendMessageForm';
+
+export function App() {
+  const { user } = useContext(AuthContext);
+  return (
+    <main
+      className={`${styles.contentWrapper} ${
+        !!user ? styles.contentSigned : ''
+      }`}
+    >
+      <MessageList />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
+    </main>
+  );
+}
